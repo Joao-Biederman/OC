73cf59ac6cf2c547d96ac44a32a2402bb9bfc7fa
Joao-Biederman <joaobiedetman@gmail.com>
2022-10-22 18:37:14 -0300

    roulette

---
+++ GA/README.md
@@ -0,0 +1,1 @@
+The codes in this repository were made for a college work in which we had to create a genetic algorithm.
---
+++ GA/main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import random
+
+def roleta(fitness):
+    rouleta = [] # criar lista para receber os elementos
+    for i in range(0, len(fitness)): #pra rodar até os o ultimo individuo da população 
+        prop = round((fitness[i]*100)/np.sum(fitness)) #calcula a porcentagem/quantidade de posisoes de cada individuo  
+        for j in range(0, prop): 
+            rouleta.append(i) #pra incrementar os individuos segundo a porcentagem
+    return random.choice(rouleta)
