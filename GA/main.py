55b79a9add228b5768426de0af8598949d2c8c75
Leonardo Calsavara <calsavara2511@gmail.com>
2022-10-31 14:31:49 -0300

    Inicio da implementação do crossover

--- GA/main.py
+++ GA/main.py
@@ -16,8 +16,23 @@ def roleta(fitness):
     pais = [P1, P2]
     return pais
 
+def blx(Pai, Mae):
+    alpha = 0.5
+    beta = random.uniform(-alpha,1+alpha)
+    print(beta)
+
+    return (Pai+beta*(Mae-Pai))
+
+    
+
 class individual:
     
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+fit = (20, 32, 8, 40)
+pais = roleta(fit)
+Pai, Mae = pais[0], pais[1]
+filho1, filho2 = blx(fit[Pai],fit[Mae]), blx(fit[Pai],fit[Mae]) 
+print(pais, fit[Pai], fit[Mae], filho1, filho2)
